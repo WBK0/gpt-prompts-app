896d03aefd1e777c0108ce99a8a6cdc5e4f0cb70
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-08-19 12:52:47 +0200

    Updated prompts component on home page

--- app/components/Prompts/Prompts.tsx
+++ app/components/Prompts/Prompts.tsx
@@ -1,15 +1,46 @@
 import React from 'react'
-import Image from 'next/image';
 
 const Prompts = () => {
   return (
-    <div className='flex flex-wrap'>
-      <h2 className='text-5xl font-gilroyHeavy w-full text-center text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-rose-500 drop-shadow'>PROMTS</h2>
-      <div className='py-10 w-full mt-12 bg-white px-3'>
-        <h4 className='font-gilroyBold text-2xl text-gray-800 text-center'>Prompt for generation a blog post for the blog with the new trends</h4>
-        <p className='font-gilroyMedium text-lg mt-4'>Explore the concept of a minimalist approach to fashion - how to curate chic and cohesive outfits with a limited number of clothing pieces? Share your advice on crafting a capsule wardrobe, selecting essential items, and creating ...</p>
-        <Image src='https://lh3.google.com/u/0/ogw/AGvuzYZtBfaYDa925u4oCRvYqV8P4GVC06xtCmg5LRN_=s32-c-mo' width={64} height={64} className='mt-5 rounded-full' alt='user icon' />
-        <span>tets</span>
+    <div className='flex flex-col items-center'>
+      <h2 className='text-5xl font-gilroyHeavy text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-rose-500 drop-shadow p-4'>MOST POPULAR</h2>
+      <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-8 mx-3'>
+        <div className='bg-white rounded-lg shadow-md flex flex-col justify-between'>
+          <div>
+            <h3 className='text-lg font-gilroyBold text-gray-900 p-4 pb-1'>How to create a successful social media marketing campaign</h3>
+            <p className='text-sm font-gilroyLight text-gray-500 p-4 pb-0'>#socialmedia #marketing #campaign</p>
+            <div className='p-4'>
+              <p className='text-sm font-gilroyLight text-gray-600 mt-1'>Learn how to create a social media marketing campaign that will help you reach your target audience and achieve your business goals.</p>
+            </div>
+          </div>
+          <div className='flex justify-end items-center p-4'>
+            <button className='bg-pink-500 hover:bg-pink-600 text-white font-medium py-2 px-4 rounded-lg'>Read more</button>
+          </div>
+        </div>
+        <div className='bg-white rounded-lg shadow-md flex flex-col justify-between'>
+          <div>
+            <h3 className='text-lg font-gilroyBold text-gray-900 p-4 pb-1'>How to create a successful social media marketing campaign</h3>
+            <p className='text-sm font-gilroyLight text-gray-500 p-4 pb-0'>#socialmedia #marketing #campaign</p>
+            <div className='p-4'>
+              <p className='text-sm font-gilroyLight text-gray-600 mt-1'>Learn how to create a social media marketing campaign that will help you reach your target audience and achieve your business goals.</p>
+            </div>
+          </div>
+          <div className='flex justify-end items-center p-4'>
+            <button className='bg-pink-500 hover:bg-pink-600 text-white font-medium py-2 px-4 rounded-lg'>Read more</button>
+          </div>
+        </div>
+        <div className='bg-white rounded-lg shadow-md flex flex-col justify-between'>
+          <div>
+            <h3 className='text-lg font-gilroyBold text-gray-900 p-4 pb-1'>How to create a successful social media marketing campaign</h3>
+            <p className='text-sm font-gilroyLight text-gray-500 p-4 pb-0'>#socialmedia #marketing #campaign</p>
+            <div className='p-4'>
+              <p className='text-sm font-gilroyLight text-gray-600 mt-1'>Learn how to create a social media marketing campaign that will help you reach your target audience and achieve your business goals.</p>
+            </div>
+          </div>
+          <div className='flex justify-end items-center p-4'>
+            <button className='bg-pink-500 hover:bg-pink-600 text-white font-medium py-2 px-4 rounded-lg'>Read more</button>
+          </div>
+        </div>
       </div>
     </div>
   )
--- app/components/Searchbar/Buttons.tsx
+++ app/components/Searchbar/Buttons.tsx
@@ -3,13 +3,15 @@ import React from 'react'
 // Buttons component - displays the buttons for searching prompts and saved prompts
 const Buttons = () => {
   return (
-    <div className='mt-7 flex gap-3'>
-      <button className='w-1/2 bg-gradient-to-br from-blue-600 to-blue-800 py-3 rounded-xl text-white font-gilroyBold'>
-        Search
-      </button>
-      <button className='w-1/2 bg-gradient-to-br from-blue-600 to-blue-800 py-3 rounded-xl text-white font-gilroyBold'>
-        Saved prompts
-      </button>
+    <div className='flex flex-col items-center'>
+      <div className='flex flex-col sm:flex-row mt-6'>
+        <button className='bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg shadow-md mb-4 sm:mb-0 sm:mr-4 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
+          Saved prompts
+        </button>
+        <button className='bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
+          Search prompts
+        </button>
+      </div>
     </div>
   )
 }
