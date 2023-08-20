2e5553d730a4ba81c5ea252adeaa9198b5765f24
Bartłomiej <bartlomiej.ostojski@teb.edu.pl>
2023-08-20 16:48:05 +0200

    Added single prompt site

--- app/components/Searchbar/Buttons.tsx
+++ app/components/Searchbar/Buttons.tsx
@@ -8,7 +8,10 @@ const Buttons = () => {
         <button className='bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg shadow-md mb-4 sm:mb-0 sm:mr-4 transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
           Saved prompts
         </button>
-        <button className='bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110'>
+        <button 
+          className='bg-blue-600 hover:bg-blue-700 text-white font-medium py-2 px-4 rounded-lg shadow-md transition duration-300 ease-in-out transform hover:-translate-y-1 hover:scale-110'
+          type='submit'  
+        >
           Search prompts
         </button>
       </div>
---
+++ app/prompt/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PromptSite = () => {
+  return (
+    <div>PromptSite</div>
+  )
+}
+
+export default PromptSite;
